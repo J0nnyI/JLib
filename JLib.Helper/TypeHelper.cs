825109e7f0babe4550129eb8cbafd3b18a729da3
Daniel Dengler <daniel@busitec.de>
2024-12-09 19:15:20 +0100

    Improve caching mechanism in TypeHelper's FullName method
    Revised the FullName method to enhance caching efficiency.
    Now, it first checks the cache with TryGetValue and only builds
    the full name if not found. Added a lock statement to ensure
    thread safety when updating the cache.

--- JLib.Helper/TypeHelper.cs
+++ JLib.Helper/TypeHelper.cs
@@ -334,7 +334,16 @@ public static class TypeHelper
     {
         // full name is frequently called and compute intensive.
         // this justifies the use of a cache
-        return FullNameCache.GetValueOrAdd((type, includeNamespace), () => FullNameBuilder(type).ToString());
+        if (FullNameCache.TryGetValue((type, includeNamespace), out var typeFullName))
+            return typeFullName;
+        else
+        {
+            typeFullName = FullNameBuilder(type).ToString();
+            lock (FullNameCache)
+                FullNameCache[(type, includeNamespace)] = typeFullName;
+            return typeFullName;
+        }
+
 
         StringBuilder FullNameBuilder(Type type)
         {
