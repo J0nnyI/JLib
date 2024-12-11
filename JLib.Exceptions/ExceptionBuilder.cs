c929cf893f223f705783758749f09b95f76d7585
Marcel Rehkemper <rehkemper@busitec.de>
2024-12-11 14:48:31 +0100

    comment fixed

--- JLib.Exceptions/ExceptionBuilder.cs
+++ JLib.Exceptions/ExceptionBuilder.cs
@@ -65,7 +65,7 @@ public sealed class ExceptionBuilder : IExceptionProvider, IDisposable
     }
 
     private bool UnlockedHasErrors()
-        // improves performance by checking the count before executing any to improve performance
+        // improves performance by checking the count before executing any
         => _exceptions.Count != 0 || (_children.Count != 0 && _children.Any(c => c.HasErrors()));
     /// <summary>
     /// builds the exception based on the current builder state.
