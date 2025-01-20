0b70db760fa07a87d9c6589e0ded2887941aa976
Marcel Rehkemper <rehkemper@busitec.de>
2025-01-20 12:48:36 +0100

    refactor: moving classes to matching files

--- JLib.Reflection/TypeValidationContextExtensions.cs
+++ JLib.Reflection/TypeValidationContextExtensions.cs
@@ -5,19 +5,9 @@ using JLib.ValueTypes;
 
 namespace JLib.Reflection;
 
-internal sealed class TypeValidationContext : ValidationContext<Type>
-{
-    private readonly TypeValueType _valueType;
-
-    public TypeValidationContext(TypeValueType valueType, Type targetType) : base(valueType.Value, targetType)
-    {
-        _valueType = valueType;
-    }
-
-    protected override string GetExceptionMessage()
-        => $"{_valueType.Value.FullName(true)} is not a valid {_valueType.GetType().FullName(true)}";
-}
-
+/// <summary>
+/// Extension methods for validating <see cref="Type"/>s
+/// </summary>
 public static class TypeValidationContextExtensions
 {
     /// <summary>
