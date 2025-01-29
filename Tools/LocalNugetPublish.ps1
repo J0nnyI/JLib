f02301a1a15fe80d6e7b0fb788e88805013b08d2
Marcel Rehkemper <rehkemper@busitec.de>
2025-01-29 06:41:56 +0100

    added a utility script for local nuget publish

---
+++ Tools/LocalNugetPublish.ps1
@@ -0,0 +1,9 @@
+# mkdir  \NuGet\LocalPackages
+cd ..
+Remove-Item .\\bin\\local\\* -r -force 
+dotnet pack -c Release -o .\\bin\\local --version-suffix 99.5.0
+cd .\\bin\\local
+#nuget delete * 99.0.0 -Verbosity detailed -noninteractive -source C:\\NuGet\\LocalPackages 
+Remove-Item C:\\NuGet\\LocalPackages\\JLib* -Force
+dotnet nuget push * --source C:\\NuGet\\LocalPackages
+cd ../../tools
