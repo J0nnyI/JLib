134840baaa3581a32f47bd296e72037bb6f7a5fe
Marcel Rehkemper <rehkemper@busitec.de>
2025-02-14 15:39:39 +0100

    raising min version to dotnet 8, updating aspnetcore, HotChocolate and FluentAssertions to the latest applicable version No, we will not use FluentAssertions 8.

--- JLib.HotChocolate/Helper/RequestExecutorBuilderHelper.cs
+++ JLib.HotChocolate/Helper/RequestExecutorBuilderHelper.cs
@@ -30,6 +30,11 @@ public abstract record TypeExtensionType : TypeValueType
 /// </summary>
 [TvtFactoryAttribute.IsClass, TvtFactoryAttribute.HasAttribute(typeof(ExtendObjectTypeAttribute))]
 public record AttributeTypeExtensionType(Type Value) : TypeExtensionType(Value);
+/// <summary>
+/// a type which extends a given graphql object.
+/// </summary>
+[TvtFactoryAttribute.IsClass, TvtFactoryAttribute.HasAttribute(typeof(ExtendObjectTypeAttribute<>))]
+public record GenericAttributeTypeExtensionType(Type Value) : TypeExtensionType(Value);
 
 
 /// <summary>
@@ -55,43 +60,4 @@ public static class RequestExecutorBuilderHelper
         return builder;
     }
 
-    /// <summary>
-    /// <b>WARNING!</b> This method must be called after <b>ALL</b> DataProvider have been registered!
-    /// </summary>
-    public static IRequestExecutorBuilder RegisterDataProvider(this IRequestExecutorBuilder builder)
-    {
-        foreach (var dataProviderRType in builder.Services
-                     .Select(x => x.ServiceType)
-                     .Where(x => x.ImplementsAny<IDataProviderR<IDataObject>>())
-                     .ToArray())
-            builder.RegisterService(dataProviderRType);
-        foreach (var dataProviderRwType in builder.Services
-                     .Select(x => x.ServiceType)
-                     .Where(x => x.ImplementsAny<IDataProviderRw<IEntity>>())
-                     .ToArray())
-            builder.RegisterService(dataProviderRwType);
-        return builder;
-    }
-
-    private static readonly MethodInfo RegisterServiceMethod =
-        typeof(RequestExecutorBuilderExtensions)
-            .GetMethod(nameof(RequestExecutorBuilderExtensions.RegisterService))
-        ?? throw new InvalidSetupException(
-            $"{nameof(IRequestExecutorBuilder)}.{nameof(RequestExecutorBuilderExtensions.RegisterService)}Method Info not found");
-    /// <summary>
-    /// <inheritdoc cref="RequestExecutorBuilderExtensions.RegisterService{TService}"/>
-    /// </summary>
-    /// <param name="builder">The <see cref="IRequestExecutorBuilder"/>.</param>
-    /// <param name="service">The service type.</param>
-    /// <param name="serviceKind">The service kind defines the way a service is injected and handled by the execution engine.</param>
-    /// <returns>
-    /// An <see cref="IRequestExecutorBuilder"/> that can be used to configure a schema
-    /// and its execution.
-    /// </returns>
-    public static IRequestExecutorBuilder RegisterService(
-        this IRequestExecutorBuilder builder, Type service, ServiceKind serviceKind = ServiceKind.Default)
-    {
-        RegisterServiceMethod.MakeGenericMethod(service).Invoke(null, new object[] { builder, serviceKind });
-        return builder;
-    }
 }
