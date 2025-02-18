72584623a0e2ceefbc6d3b95301705f48f7e75b0
Marcel Rehkemper <rehkemper@busitec.de>
2025-02-18 18:10:20 +0100

    DataProviderR Documentation

--- JLib.DataProvider/DataProviderInterfaces/IDataProviderR.cs
+++ JLib.DataProvider/DataProviderInterfaces/IDataProviderR.cs
@@ -1,14 +1,32 @@
 ﻿namespace JLib.DataProvider;
 
+/// <summary>
+/// Provides a common way to access <see cref="IDataObject"/>s of any kind using <see cref="Guid"/> Ids.<br/>
+/// Basically an automated Repository
+/// </summary>
+/// <typeparam name="TDataObject"></typeparam>
 public interface IDataProviderR<TDataObject>
     where TDataObject : IDataObject
 {
+    /// <returns>a queryable with all <see cref="TDataObject"/>s of this DataProvider</returns>
     public IQueryable<TDataObject> Get();
 
+    /// <param name="id">the <see cref="IDataObject.Id"/> of the requested <typeparamref name="TDataObject"/> </param>
+    /// <returns>the  requested <typeparamref name="TDataObject"/> with <see cref="IDataObject.Id"/> <paramref name="id"/>.
+    /// Throws an <see cref="DataProviderException.DataExceptions.EntityNotFoundException{TDataObject}"/> if it could not be found.</returns>
+    /// <exception cref="DataProviderException.DataExceptions.EntityNotFoundException{TDataObject}"/>
     public TDataObject Get(Guid id);
 
+    /// <param name="ids">the <see cref="IDataObject.Id"/> of the requested <typeparamref name="TDataObject"/>s.</param>
+    /// <returns>the  requested <typeparamref name="TDataObject"/>s per <see cref="IDataObject.Id"/></returns>
+    /// <exception cref="DataProviderException.DataExceptions.EntityNotFoundException{TDataObject}"/>
     public IReadOnlyDictionary<Guid, TDataObject> Get(IReadOnlyCollection<Guid> ids);
 
+    /// <param name="id">the <see cref="IDataObject.Id"/> of the requested <typeparamref name="TDataObject"/> </param>
+    /// <returns>The  requested <typeparamref name="TDataObject"/> with <see cref="IDataObject.Id"/> <paramref name="id"/> or null, if it could not be found</returns>
+    /// <exception cref="DataProviderException.DataExceptions.EntityNotFoundException{TDataObject}"/>
     public TDataObject? TryGet(Guid? id);
+    /// <param name="id">the <see cref="IDataObject.Id"/> to look for.</param>
+    /// <returns>true, if an entity of type <typeparamref name="TDataObject"/> could be found.</returns>
     public bool Contains(Guid? id);
 }
